ea7601c45e43860f205ef9827f1dcd83aaf9e328
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-11-05 21:11:53 +0900

    Update DataWriterSDCard.ino
    I need to limit the letter depth but I will figure this out tomorrow, might actually do it directly on the python code Data to txt.py

--- Setup Code for SD card/DataWriterSDCard.ino
+++ Setup Code for SD card/DataWriterSDCard.ino
@@ -2,77 +2,100 @@
 #include <SPI.h>
 
 const int CS_pin = 43;
-int longeur_data = 8 //nombre d'element dans une ligne
-int nombre_total_ligne = 40 //nombre total de ligne qui vont etre inprime
-int num_ligne = 1;    // Line number to read
+int length_data = 9; // nombre d'éléments dans une ligne
+int nombre_total_ligne = 40; // nombre total de lignes qui vont être imprimées
+int num_ligne = 1; // Line number to read
 String phrase = "";
 
 void setup() {
-  Serial.begin(9600);
-  if (!SD.begin(CS_pin)) {
-    Serial.println("Initialization failed!");
-    return;
-  }
-  Serial.println("SD card initialized.");
+    Serial.begin(9600);
+    
+    if (!SD.begin(CS_pin)) {
+        Serial.println("Initialization failed!");
+        return;
+    }
+    
+    Serial.println("SD card initialized.");
 }
 
 void loop() {
-  //isolement de la premiere phrase
-  if(num_ligne <= nombre_total_ligne){
-  File dataFile = SD.open("PRACTICE.txt",FILE_READ);
-  if (dataFile) {
-    int ligne_actuelle = 1;     // Current line counter
-    phrase = "";
+    // Isolement de la première phrase
+    if (num_ligne <= nombre_total_ligne) {
+        File dataFile = SD.open("PRACTICE.txt", FILE_READ);
+        
+        if (dataFile) {
+            int ligne_actuelle = 1; // Current line counter
+            phrase = "";
 
-    while (dataFile.available()) {
-      char c = dataFile.read();
-      if (c == '\n') {
-        // If we reach the desired line number, stop reading
-        if (ligne_actuelle == num_ligne) break;
-        // Move to the next line
-        ligne_actuelle++;
-        phrase = "";  // Reset phrase for the next line
-      } else if (ligne_actuelle == num_ligne) {
-        // Append character to phrase if we’re on the desired line
-        phrase += c;
-      }
-    }
+            while (dataFile.available()) {
+                char c = dataFile.read();
+                
+                if (c == '\n') {
+                    // If we reach the desired line number, stop reading
+                    if (ligne_actuelle == num_ligne) break;
+                    // Move to the next line
+                    ligne_actuelle++;
+                    phrase = ""; // Reset phrase for the next line
+                } else if (ligne_actuelle == num_ligne) {
+                    // Append character to phrase if we’re on the desired line
+                    phrase += c;
+                }
+            }
 
-    dataFile.close();
+            dataFile.close();
 
-    //isolement du premier mot
-    String firstWord = "";
-    for (char c : phrase) {
-        if (c == ' ') break; // Stop when a space is encountered
-        firstWord += c;       // Add each character to firstWord
-    }
+            // Isolement du premier mot
+            String firstWord = "";
+            for (char c : phrase) {
+                if (c == ' ') break; // Stop when a space is encountered
+                firstWord += c; // Add each character to firstWord
+            }
 
-    //creation des differents sous directory
-    String directoryPath = "";
-    for (int i = 0; i < firstWord.length(); i++){
-    char lettre = firstWord[i];
-    if (!SD.exists(directoryPath+"/"+lettre)){
-    SD.mkdir(directoryPath+"/"+lettre);}
-    directoryPath = directoryPath+"/"+lettre;
-}
-File NewFile = SD.open(directoryPath+"/"+firstWord, FILE_WRITE);
+            // Creation des différents sous répertoires
+            String directoryPath = "";
+            for (int i = 0; i < firstWord.length(); i++) {
+                char lettre = firstWord[i];
+                if (!SD.exists(directoryPath + "/" + lettre)) {
+                    SD.mkdir(directoryPath + "/" + lettre);
+                    Serial.println("New directory at " + directoryPath + " was created named " + lettre);
+                } else {
+                    Serial.println("Directory of path " + directoryPath + "/" + lettre + " already existed");
+                }
+                directoryPath = directoryPath + "/" + lettre;
+            }
 
-//I need to add a line by line thing here instead of just writing down all the data
+            File NewFile = SD.open(directoryPath + "/" + firstWord, FILE_WRITE);
+            delay(50); // 50 ms delay
 
-for (int i = 1; i <= longueur_data; i++) {
-  String mot = "";
-  int indice_derniere_lettre = 0;
-  while (phrase[indice_derniere_lettre] != ' ') {
-    mot += phrase[indice_derniere_lettre];
-    indice_derniere_lettre += 1;
-  }
-  indice_derniere_lettre += 1;
-  NewFile.println(mot);
-}
-NewFile.close();
+            // I need to add a line by line thing here instead of just writing down all the data
+            if (NewFile) {
+                Serial.println("Making of new file successful");
+                int indice_derniere_lettre = 0;
 
+                for (int i = 1; i <= length_data; i++) {
+                    String mot = "";
+
+                    while (phrase[indice_derniere_lettre] != ' ') {
+                        mot += phrase[indice_derniere_lettre];
+                        indice_derniere_lettre += 1;
+                    }
+                    indice_derniere_lettre += 1;
+                    NewFile.println(mot);
+                }
+                NewFile.close();
+                delay(50);
+            } else {
+                Serial.println("Error making new file");
+            }
+        } else {
+            Serial.println("Error opening file.");
+        }
+
+        num_ligne += 1; // Increment line number for the next loop iteration
     } else {
-    Serial.println("Error opening file.");
-  }
-  num_ligne += 1;  // Increment line number for the next loop iteration
-}}
+        Serial.println("Finished!");
+        while (true) {
+            delay(1000);
+        }
+    }
+}
