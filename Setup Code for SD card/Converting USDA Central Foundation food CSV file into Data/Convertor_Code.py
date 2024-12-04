61bad91b25ade6dcdeab3b7dd1fbfbccf5ab1bb7
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-12-05 08:22:31 +0900

    Delete Setup Code for SD card/Converting USDA Central Foundation food CSV file into Data/Convertor_Code.py

--- Setup Code for SD card/Converting USDA Central Foundation food CSV file into Data/Convertor_Code.py
+++
@@ -1,7 +0,0 @@
-import CSV
-
-with open(r"C:\Users\dmlam\Downloads\FoodData_Central_foundation_food_csv_2024-10-31\foundation_food.csv", mode='r') as file:
-    reader = csv.reader(file)
-	foundation_food = []
-    for row in reader:
-        foundation_food.append([row[0],row[1]])
